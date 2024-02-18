8f9cbc23100e0497f0e5940b41106b48a587fd67
khal 45 <khalifaariko@gmail.com>
2024-02-18 00:04:28 +0000

    feat: build stuff

--- myApp.js
+++ myApp.js
@@ -1,11 +1,42 @@
 require('dotenv').config();
-
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
-};
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = async (done) => {
+  const khal45 = new Person({name: "Khal 45", age: 20, favoriteFoods: ["rice", "meat"]});
+  (async () => {
+    try {
+      const result = await khal45.save();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+  
+  // fcc code below
+
+  // khal45.save((err, data) => {
+  //   if (err) return console.error(err);
+  //   done(null, data)
+  // });
+  // console.log(`${khal45} 45`);
+}
+// const createAndSavePerson = (done) => {
+//   done(null /*, data*/);
+// };
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
